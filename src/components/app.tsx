16c45d3b7b01eb8c0396e518f7a80fbffd67faff
EgorGER <QwertiQQQ@gmail.com>
2024-04-19 22:02:40 +0300

    добовляет страничку авторизации

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter, Routes} from 'react-router-dom';
+import Login from '../pages/login/login';
 import MainPage from '../pages/main-page/main-page';
 import Quest from '../pages/quest/quest';
 import Contacts from '../pages/contacts/contacts';
@@ -8,6 +9,7 @@ function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path='/' element={<MainPage />} />
         <Route path='/quest' element={<Quest />} />
         <Route path='/contacts' element={<Contacts />} />
