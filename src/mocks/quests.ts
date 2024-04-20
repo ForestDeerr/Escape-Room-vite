798edec29cee538b34fc937d435f9a86d8f472f1
EgorGER <QwertiQQQ@gmail.com>
2024-04-20 20:55:41 +0300

    создает моки

---
+++ src/mocks/quests.ts
@@ -0,0 +1,26 @@
+import { QuestsType } from '../types/quests';
+
+export const quests:QuestsType = [
+  {
+    id: 'aba664c3-bdf3-4fb3-b8f3-42e007864bbf',
+    title: 'Склеп',
+    previewImg: 'img/content/crypt/crypt-size-s.jpg',
+    previewImgWebp: 'https://{server-url}/static/quest/sklep.webp',
+    level: 'easy',
+    type: 'adventures',
+    peopleMinMax: [
+      5
+    ]},
+  {
+    id: 'aba664c3-bdf3-4fb3-b8f3-42e007864bbg',
+    title: 'Эксперемент',
+    previewImg: 'img/content/experiment/experiment-size-s.jpg',
+    previewImgWebp: 'https://{server-url}/static/quest/sklep.webp',
+    level: 'easy',
+    type: 'adventures',
+    peopleMinMax: [
+      5
+    ]},
+
+
+];
---
+++ src/types/quests.ts
@@ -0,0 +1,11 @@
+export type QuestType = {
+  id: string;
+  title: string;
+  previewImg: string;
+  previewImgWebp: string;
+  level: string;
+  type: string;
+  peopleMinMax: number[];
+};
+
+export type QuestsType = QuestType[];
